dd43cfb7380860b021c49d45cec69d11376e626b
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-10-18 14:29:28 -0500

    out of cred block

--- vars/devpiTest.groovy
+++ vars/devpiTest.groovy
@@ -21,15 +21,15 @@ def call(Map args) {
     echo "Testing on ${NODE_NAME}"
 //    tester.connect()
 
-    bat "${args.devpiExecutable} use ${args.url} --clientdir ${args.certsDir}"
 
+    bat "${args.devpiExecutable} use ${args.url} --clientdir ${args.certsDir}"
     withCredentials([usernamePassword(credentialsId: "DS_devpi", usernameVariable: 'DEVPI_USERNAME', passwordVariable: 'DEVPI_PASSWORD')]) {
 //        bat "${args.devpiExecutable} login DS_Jenkins --clientdir ${args.certsDir} --password ${DEVPI_PASSWORD}"
         tester.userName = "${DEVPI_USERNAME}"
         tester.userPassword = "${DEVPI_PASSWORD}"
-        bat "${tester.buildLoginCommand()}"
-        bat "${args.devpiExecutable} use ${args.index} --clientdir ${args.certsDir}"
     }
+    bat "${tester.buildLoginCommand()}"
+    bat "${args.devpiExecutable} use ${args.index} --clientdir ${args.certsDir}"
 
     withEnv(["PYTEST_ADDOPTS=${args.pytestArgs}"]) {
         bat "${tester.buildTestCommandString()}"
