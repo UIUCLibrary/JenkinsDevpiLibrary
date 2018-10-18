c1ec66ac81586f336df6338d4dea29b5c8f76351
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-10-18 12:45:36 -0500

    DevPiTester

---
+++ src/org/ds/devpi/DevPiTester.groovy
@@ -0,0 +1,40 @@
+package org.ds.devpi
+
+class DevPiTester implements Serializable{
+    public url
+    public index
+    public pkgName
+    public pkgRegex
+    public certsDir = "certs\\"
+    public devpiExecutable
+    public pkgVersion
+
+    DevPiTester(devpiExecutable) {
+        this.devpiExecutable = devpiExecutable
+    }
+
+    def buildTestCommandString(){
+        def command = ""
+
+        if(!devpiExecutable){
+            throw Exception("No path to devpi Executable defined")
+        }
+        command += devpiExecutable
+
+        command += " --index " + index
+        command += " " + pkgName
+
+        if(pkgVersion){
+            command += "==" + pkgVersion
+        }
+
+        if(pkgRegex){
+            command += " -s " + pkgRegex
+        }
+
+        if(certsDir){
+            command += " --clientdir " + certsDir
+        }
+        return  command
+    }
+}
