b6724d57cc0cf33ada9b04f0a3ee84f4b42148a4
henry <henryborchers@yahoo.com>
2020-06-11 14:34:18 -0500

    use sh on unix

--- vars/devpiTest.groovy
+++ vars/devpiTest.groovy
@@ -33,33 +33,54 @@ def call(Map args) {
         tester.userPassword = "${DEVPI_PASSWORD}"
     }
     echo "Testing on ${NODE_NAME}"
-
-    def use_command = "${tester.buildUseCommand()}".replace("\\", "\\\\")
-    bat(
-        script: "${use_command}",
-        label: "Configuring DevPi to use server ${tester.url}",
-    )
-
-//    For some reason I need to remove any ^ characters
-//    Escape $ characters
-    def logginCommand = "${tester.buildLogInCommand()}".replace("\$", "`\$").replace("^", "")
-    powershell(
-        label: "Logging into DevPi server",
-        script: "& ${logginCommand}"
-    )
-
-    def index_command = "${tester.buildSelectIndexCommand()}".replace("\\", "\\\\")
-    bat(
-        label: "Selecting DevPi index, ${tester.index}",
-        script: "${index_command}"
-    )
-
-    def test_command = "${tester.buildTestCommandString()}".replace("\\", "\\\\")
-    withEnv(["PYTEST_ADDOPTS=${args.pytestArgs}"]) {
-        bat(
-            label: "Running Tests DevPi packages ${tester.pkgName}, version ${tester.pkgVersion} with ${args.pkgRegex}",
-            script: test_command
+    if(isUnix()){
+        sh(script: "${use_command}",
+           label: "Configuring DevPi to use server ${tester.url}",
         )
+        def logginCommand = "${tester.buildLogInCommand()}".replace("\$", "`\$").replace("^", "")
+        sh(label: "Logging into DevPi server",
+           script: "& ${logginCommand}"
+        )
+        def index_command = "${tester.buildSelectIndexCommand()}".replace("\\", "\\\\")
+        sh(label: "Selecting DevPi index, ${tester.index}",
+           script: "${index_command}"
+        )
+
+        def test_command = "${tester.buildTestCommandString()}".replace("\\", "\\\\")
+        withEnv(["PYTEST_ADDOPTS=${args.pytestArgs}"]) {
+            sh(label: "Running Tests DevPi packages ${tester.pkgName}, version ${tester.pkgVersion} with ${args.pkgRegex}",
+               script: test_command
+            )
+        }
+    } else {
+        def use_command = "${tester.buildUseCommand()}".replace("\\", "\\\\")
+        bat(
+            script: "${use_command}",
+            label: "Configuring DevPi to use server ${tester.url}",
+        )
+
+    //    For some reason I need to remove any ^ characters
+    //    Escape $ characters
+        def logginCommand = "${tester.buildLogInCommand()}".replace("\$", "`\$").replace("^", "")
+        powershell(
+            label: "Logging into DevPi server",
+            script: "& ${logginCommand}"
+        )
+
+        def index_command = "${tester.buildSelectIndexCommand()}".replace("\\", "\\\\")
+        bat(
+            label: "Selecting DevPi index, ${tester.index}",
+            script: "${index_command}"
+        )
+
+        def test_command = "${tester.buildTestCommandString()}".replace("\\", "\\\\")
+        withEnv(["PYTEST_ADDOPTS=${args.pytestArgs}"]) {
+            bat(
+                label: "Running Tests DevPi packages ${tester.pkgName}, version ${tester.pkgVersion} with ${args.pkgRegex}",
+                script: test_command
+            )
+        }
     }
+
 
 }
