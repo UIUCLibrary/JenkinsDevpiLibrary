844cadc10bed67bd2bc02a92aaa4eb89433becc1
henry <henryborchers@yahoo.com>
2020-06-11 15:00:41 -0500

    debug

--- vars/devpiTest.groovy
+++ vars/devpiTest.groovy
@@ -34,6 +34,7 @@ def call(Map args) {
     }
     echo "Testing on ${NODE_NAME}"
     if(isUnix()){
+        echo "This is unix"
 //        def use_command = "${tester.buildUseCommand()}".replace("\\", "\\\\")
         sh(label: "Configuring DevPi to use server ${tester.url}",
            script: tester.buildUseCommand(),
